1995b78cddacc9833a372a7e2ce025d3f5311ce7
cipher67 <mrunalshinde52@gmail.com>
2023-06-13 17:01:54 +0530

    first

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+      /* don't expose password or any sensitive info, done only for demo */
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "freedbtech_language",
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
